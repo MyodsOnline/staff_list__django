c7ff64317a51e9c2ee1e4f8a167c1e6603cf7229
Andrew Smith <diver.vlz@gmail.com>
2022-05-25 23:57:12 +0300

    add script.js

--- staff_server/staff_list/static/script/script.js
+++ staff_server/staff_list/static/script/script.js
@@ -1,2 +1,14 @@
 
-console.log('kjdnskjnkjcn')
+const buttons = document.querySelectorAll(".object_item_title");
+
+const handleClick = (event) => {
+	let el = event.target;
+	let el_p = el.parentElement.nextElementSibling.children;
+	console.log(el.innerText);
+	console.log(el_p);
+}
+
+buttons.forEach(button => {
+	button.addEventListener('click', handleClick)
+})
+
