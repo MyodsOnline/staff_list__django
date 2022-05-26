fb96eaf5f44584e828a0875b5b49315673615b16
Andrew Smith <diver.vlz@gmail.com>
2022-05-27 00:04:50 +0300

    daily update

--- staff_server/staff_list/static/script/script.js
+++ staff_server/staff_list/static/script/script.js
@@ -1,14 +1,38 @@
 
-const buttons = document.querySelectorAll(".object_item_title");
+const buttons = document.querySelectorAll(".checker");
+const staffs = document.querySelectorAll("input");
 
-const handleClick = (event) => {
+
+const handleClickTitle = (event) => {
 	let el = event.target;
-	let el_p = el.parentElement.nextElementSibling.children;
-	console.log(el.innerText);
-	console.log(el_p);
+	el.classList.toggle('fa-angle-down')
+	el.classList.toggle('fa-angle-up')
+	let el_li = el.parentElement.parentElement.nextElementSibling.children;
+    for (let el_li_item of el_li) {
+		if (el_li_item.classList.contains('checked')) {
+			
+		} else {
+			el_li_item.classList.toggle('ok');
+		}
+	}
+	// console.log(el.innerText);
+}
+
+const handleClickStaff = (event) => {
+	let staff_el = event.target;
+	// console.log(staff_el)
+	let staff_el_li = staff_el.parentElement.parentElement;
+	// console.log(staff_el_li)
+	let staff_el_ul = staff_el_li.parentElement.children;
+	// console.log(staff_el_ul);
+	staff_el_li.classList.toggle('checked')
 }
 
 buttons.forEach(button => {
-	button.addEventListener('click', handleClick)
+	button.addEventListener('click', handleClickTitle)
+})
+
+staffs.forEach(staff => {
+	staff.addEventListener('click', handleClickStaff)
 })
 
